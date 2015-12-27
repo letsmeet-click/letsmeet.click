7a602cf49a9eae68ea8b846e558fcbc65ecb2d61
Andreas Madsack <andreas@madflex.de>
2015-12-27 17:53:10 +0100

    add login with twitter

--- letsmeet/letsmeet/settings/base.py
+++ letsmeet/letsmeet/settings/base.py
@@ -129,5 +129,6 @@ LOGIN_REDIRECT_URL = 'home'
 AUTHENTICATION_BACKENDS = (
     'rules.permissions.ObjectPermissionBackend',
     'social.backends.github.GithubOAuth2',
+    'social.backends.twitter.TwitterOAuth',
     'django.contrib.auth.backends.ModelBackend',
 )
