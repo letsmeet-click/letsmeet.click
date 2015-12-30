c9655427f9b3cfb320be56fe194cc6454110d48d
Andreas Madsack <andreas@madflex.de>
2015-12-30 14:48:45 +0100

    easier way to get osm cdn

--- letsmeet/locations/admin.py
+++ letsmeet/locations/admin.py
@@ -1,28 +1,12 @@
 from django.contrib.gis import admin
-from django import forms
-from django.contrib.gis.forms import OpenLayersWidget
 
 from .models import Location
 
 
-class CDNOpenLayersWidget(OpenLayersWidget):
-    class Media:
-        js = (
-            'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/OpenLayers.js',
-            'gis/js/OLMapWidget.js',
-        )
-
-
-class LocationAdminForm(forms.ModelForm):
-    class Meta:
-        model = Location
-        fields = ['name', 'slug', 'geo_location', 'description', 'notes', 'city', 'country']
-        widgets = {
-            'geo_location': (CDNOpenLayersWidget),
-        }
+class CDNOSMGeoAdmin(admin.OSMGeoAdmin):
+    openlayers_url = 'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/OpenLayers.js'
 
 
 @admin.register(Location)
-class LocationAdmin(admin.OSMGeoAdmin):
-    form = LocationAdminForm
+class LocationAdmin(CDNOSMGeoAdmin):
     list_display = ['name', 'city', 'country']
