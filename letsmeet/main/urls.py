6190cc2ae5f355feef2739ce5590a7f8ef0c39c0
Arne Schauf <git@as-webservices.de>
2016-01-04 20:13:39 +0100

    move templates into main subfolder for better structure

--- letsmeet/main/urls.py
+++ letsmeet/main/urls.py
@@ -6,9 +6,9 @@ from django.conf import settings
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
-    url(r'^legal/$', TemplateView.as_view(template_name='legal.html'), name='legal'),
+    url(r'^$', TemplateView.as_view(template_name='main/home.html'), name='home'),
+    url(r'^contact/$', TemplateView.as_view(template_name='main/contact.html'), name='contact'),
+    url(r'^legal/$', TemplateView.as_view(template_name='main/legal.html'), name='legal'),
     url(r'^login/$', auth_views.login, {'template_name': 'main/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout_then_login, name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
