3c9b5ec8477904b8713ed819ca0058bbed780c83
Andreas Madsack <andreas@madflex.de>
2016-01-08 17:38:25 +0100

    pimp the admin for communities

--- letsmeet/communities/admin.py
+++ letsmeet/communities/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from .models import (
     Community, CommunitySubscription,
@@ -15,8 +16,13 @@ class CommunityAdmin(admin.ModelAdmin):
     inlines = [
         CommunitySubscriptionInline,
     ]
-    list_display = ['name', 'subscribers', 'is_deleted']
+    list_display = ['name', 'show_subscriber_count', 'is_deleted']
     list_filter = ['is_deleted']
 
+    def show_subscriber_count(self, obj):
+        return obj.subscriber_count
+    show_subscriber_count.admin_order_field = 'subscriber_count'
+    show_subscriber_count.short_description = 'subscriber count'
+
     def get_queryset(self, request):
-        return self.model.default.get_queryset()
+        return self.model.default.get_queryset().annotate(subscriber_count=Count('subscribers'))
