4ce55ba8693c897bf3dfe41012451483af519a99
Michael Käufl <letsmeet-click@c.michael-kaeufl.de>
2017-06-28 21:28:35 +0200

    [tests] Restructured tests of `/`

--- letsmeet/tests/test_home.py
+++ letsmeet/tests/test_home.py
@@ -1,17 +1,22 @@
+import pytest
 
-def test_home(client):
+
+@pytest.fixture
+def generic_menu_items():
+    return ['Home', 'All Communities', 'Contact']
+
+
+def test_home(client, generic_menu_items):
     resp = client.get('/')
     assert resp.status_code == 200
-    assert b'Login' in resp.content
-    assert b'Home' in resp.content
-    assert b'Communities' in resp.content
-    assert b'Contact' in resp.content
+
+    for menu_item in generic_menu_items + ['Login']:
+        assert menu_item.encode() in resp.content
 
 
-def test_home_logged_in(logged_in_client):
+def test_home_logged_in(logged_in_client, generic_menu_items):
     resp = logged_in_client.get('/')
     assert resp.status_code == 200
-    assert b'Logout' in resp.content
-    assert b'Home' in resp.content
-    assert b'Communities' in resp.content
-    assert b'Contact' in resp.content
+
+    for menu_item in generic_menu_items + ['Logout']:
+        assert menu_item.encode() in resp.content
