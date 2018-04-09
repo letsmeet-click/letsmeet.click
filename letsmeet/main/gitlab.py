677ac4d7e89a52d442a50609646125c4633a3f1c
Andreas Madsack <andreas@madflex.de>
2018-04-09 13:22:26 +0200

    remove some shackspace specific stuff

--- letsmeet/main/gitlab.py
+++
@@ -1,8 +0,0 @@
-from social_core.backends.gitlab import GitLabOAuth2
-
-
-class ShackGitLabOAuth2(GitLabOAuth2):
-    name = 'shackgitlab'
-    API_URL = 'https://git.shackspace.de'
-    AUTHORIZATION_URL = 'https://git.shackspace.de/oauth/authorize'
-    ACCESS_TOKEN_URL = 'https://git.shackspace.de/oauth/token'
